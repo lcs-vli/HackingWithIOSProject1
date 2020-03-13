9c4357c9c24da6981c2e4d7607a2e03960135899
Victoria Li <VLi@lcs.on.ca>
2020-03-12 20:09:11 -0400

    Initial Commit

---
+++ HackingWithIOSProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackingWithIOSProject1
+//
+//  Created by Li, Muchen on 2020-03-12.
+//  Copyright © 2020 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
